a621f6309dff079da53350f2c948e3f4f6f3b245
N-McClure <64433966+N-McClure@users.noreply.github.com>
2023-02-24 10:25:21 -0500

    Add files via upload

---
+++ PHP_Intro/intro.php
@@ -0,0 +1,17 @@
+<?php
+   echo("I updated PHP."); 
+   echo("<h1>HTML Header using PHP</h1>");
+
+   $passMark = 50;
+   $myMark = 80;
+   if ($myMark >= $passMark)
+   {
+        "<p>Passed with a mark of : $myMark</p>"
+   }
+   else
+   {
+    "<p>Failed with a mark of : $myMark</p>"
+   }
+?>
+
+<h3>I am Raw HTML</h3>
